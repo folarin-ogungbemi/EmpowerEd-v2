c432e109dc01adccaddd36c08a21f72f92c6df22
folarin-ogungbemi <ftogungbemi@gmail.com>
2023-05-26 08:37:58 +0200

    Connect django and react

--- empowered/settings.py
+++ empowered/settings.py
@@ -86,7 +86,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-            os.path.join(BASE_DIR, 'templates',),
+            os.path.join(BASE_DIR, 'frontend/build',),
         ],
         'APP_DIRS': True,
         'OPTIONS': {
@@ -166,7 +166,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'frontend/build/static'),)
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
